435d70e9cd742a4d2974985ba5b3c14156160830
Jonas Engberg <jonas@dhcp-eduroam-86-50-9-126.mobile.helsinki.fi>
2019-03-28 16:02:43 +0200

    Nimen lisääminen

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Contacts from './components/Contacts'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = (props) => {
+  const [ persons, setPersons] = useState(props.contacts) 
+  const [ newName, setNewName ] = useState('')
+
+  const rows = () => persons.map(contact =>
+    <Contacts 
+    key={contact.id}
+    contact={contact}
+    />
+  )
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const contactObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+    setPersons(persons.concat(contactObject))
+    setNewName('')
   }
+
+  const handleContactChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addContact}>
+        <div>
+          nimi: <input 
+          value={newName}
+          onChange={handleContactChange}
+          />
+        </div>
+        <div>
+        <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      <ul>
+          {rows()}
+      </ul>
+    </div>
+  )
+
 }
 
-export default App;
+export default App
--- puhelinluettelo/src/index.js
+++ puhelinluettelo/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+const contacts = [
+    {
+      id: 1,
+      name: 'Jonas Engberg',
+    },
+    {
+      id: 2,
+      name: 'Niklas Pimenoff',
+    }
+  ]
+ReactDOM.render(<App contacts={contacts}/>, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
